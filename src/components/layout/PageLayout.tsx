2e5b8237098c10b1773fe50bc0af7c4754245359
Kunal Ingale <2103095@ritindia.edu>
2025-06-03 14:21:51 +0530

    updated tsconfig

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import { mockUser } from "../../data/mockData";
 
 interface PageLayoutProps {
   children: React.ReactNode;
@@ -9,12 +8,11 @@ interface PageLayoutProps {
 const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   // For demo purposes, we're using the mock user
   // In a real app, this would come from auth state
-  const user = mockUser;
 
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <Header user={user} />
+        <Header />
 
         <main className="flex-1">{children}</main>
       </div>
